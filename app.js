208cdb5021c8c2e602fa1e0cf3dff1991fc2874a
Andrea Cleland <andreaclelan@gmail.com>
2022-02-09 12:47:00 -0800

    implemented the first two queries

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-import { getDogs } from './fetch-utils.js';
+import { getDogs, getDog} from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+
 // on load
+window.addEventListener('load', async () => {
+
+} );
+
 // fetch all dogs
 // render and append all dog cards to the container
